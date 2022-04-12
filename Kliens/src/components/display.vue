2d2730360afed8b13c8f370ad40e89452fbfe059
Stoica Attila <attika_stoica@yahoo.com>
2022-04-12 15:35:28 +0300

    Display

---
+++ Kliens/src/components/display.vue
@@ -0,0 +1,64 @@
+<template>
+  <Bar :chart-data="chartData"> </Bar>
+</template>
+
+<script>
+import { Bar } from "vue-chartjs";
+
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from "chart.js";
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+);
+export default {
+  name: "BarChart",
+  components: {
+    Bar,
+  },
+  data: () => ({
+    chartData: {
+      labels: [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ],
+      datasets: [
+        {
+          label: "Data One",
+          backgroundColor: "#f87979",
+          data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11],
+        },
+      ],
+    },
+    chartOptions: {
+      responsive: true,
+      maintainAspectRatio: false,
+    },
+  }),
+  methods: {},
+};
+</script>
+
+<style></style>
