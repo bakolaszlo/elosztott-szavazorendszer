60bdaf8109d57161294b6180de721b9dd5db1adb
AnitaGabor <79316759+AnitaGabor@users.noreply.github.com>
2022-04-12 15:17:49 +0300

    Model

---
+++ Szerver/Osztott szavazasi rendszer/Osztott szavazasi rendszer/Models/Form.cs
@@ -0,0 +1,12 @@
+﻿using static Osztott_szavazasi_rendszer.Data.Enum;
+
+namespace Osztott_szavazasi_rendszer.Models
+{
+    public class Form
+    {
+        public int Id { get; set; }
+        public List<string> Questions { get; set; }
+        public List<string>? Answers { get; set; }
+        public List<QuestionType> QuestionTypes { get; set; }
+    }
+}
